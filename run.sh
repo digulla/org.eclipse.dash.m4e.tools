c95e18338fab68d388a612fabe043443637e95ca
Aaron Digulla <digulla@hepe.com>
2011-05-13 11:44:25 +0200

    Run attach-sources on the merged repository instead of running it on each one.

--- run.sh
+++ run.sh
@@ -14,7 +14,6 @@ for d in ../tmp/*_home/m2repo ; do
     if [[ "$d" = */priming_home/* ]]; then
     	continue
     fi
-    ./m4e-attach-sources.py "$d" || exit 1
     folders="$folders '$d'"
 done
 
@@ -22,6 +21,7 @@ if [[ -d ../tmp/m2repo ]]; then
     rm -r ../tmp/m2repo
 fi
 eval ./m4e-merge.py $folders ../tmp/m2repo || exit 1
+./m4e-attach-sources.py ../tmp/m2repo || exit 1
 ./m4e-apply-patches.py ../patches ../tmp/m2repo || exit 1
 ./m4e-analyze.py ../tmp/m2repo || exit 1
 ./m4e-dm.py ../tmp/m2repo org.eclipse.m4e:m4e-dependencyManagement:3.6.2 || exit 1
